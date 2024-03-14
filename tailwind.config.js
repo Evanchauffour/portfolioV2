e20660abb93b44bd94d947cc40393c8f573fe2df
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2024-03-14 11:19:06 +0100

    [Global] Add tailwind and authorized link in nextConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'text': '#020E03',
+        'primary': '#c93535',
+        'secondary': '#083134',
+        'background': '#FAFEFA',
+        'grey': '#918f8f',
+        'accent': '#FDCA40'
+      }
     },
+    fontFamily: {
+      'sora': ['var(--sora)', 'sans-serif'],
+    }
   },
   plugins: [],
 };
